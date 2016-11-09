b858eb897654bd1a9b7529fb833fc3afbc221891
Denys Honcharenko <anry200@gmail.com>
2016-11-09 20:23:00 +0200

    added very simple "local" module to project

---
+++ local/src/main/java/com/github/anry200/local/LocalHelloWorld.java
@@ -0,0 +1,9 @@
+package com.github.anry200.local;
+
+import android.util.Log;
+
+public class LocalHelloWorld {
+    public static void sayHello(String name) {
+        Log.v("LocalHelloWorld", "Hello " + name);
+    }
+}
