21184a84720c60f812614241cb0fe1f35a5974f9
Denys Honcharenko <anry200@gmail.com>
2016-11-09 20:58:51 +0200

    add simple "nexus" module

--- app/src/main/java/com/github/anry200/maven/MainActivity.java
+++ app/src/main/java/com/github/anry200/maven/MainActivity.java
@@ -3,6 +3,7 @@ package com.github.anry200.maven;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import com.github.anry200.local.LocalHelloWorld;
+import com.github.anry200.nexus.NexusHelloWorld;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         LocalHelloWorld.sayHello("Jon Dou");
+        NexusHelloWorld.sayHello("Jon Dou");
     }
 }
---
+++ nexus/src/main/java/com/github/anry200/nexus/NexusHelloWorld.java
@@ -0,0 +1,9 @@
+package com.github.anry200.nexus;
+
+import android.util.Log;
+
+public class NexusHelloWorld {
+    public static void sayHello(String name) {
+        Log.d("NexusHelloWorld", "Hello from nexus library to " + name);
+    }
+}
